5cf157c9ca24ede122306af22fdd97126bdc56bc
Abigail Samanta Elis <abigailelis.s@hotmail.com>
2025-01-11 23:05:33 -0300

    Update buttons

--- social-links-profile/main.js
+++ social-links-profile/main.js
@@ -1,41 +1,13 @@
 let card_social_links = document.getElementById('card-social-links');
 
-links = [
-    {
-        name: "GitHub",
-        link: "#"
-    },
-    {
-        name: "Frontend Mentor",
-        link: "#"
-    },
-    {
-        name: "LinkedIn",
-        link: "#"
-    },
-    {
-        name: "Twitter",
-        link: "#"
-    },
-    {
-        name: "Instagram",
-        link: "#"
-    }
-]
+links = [ "GitHub", "Frontend Mentor", "LinkedIn", "Twitter", "Instagram"];
 
 links.forEach(social_link => {
     // Crea el botón 
     let button = document.createElement('button');
-    button.type = 'button';
+    button.type = 'button'
+    button.textContent = social_link;
     button.classList.add('social-btn');
-
-    // Crea el anchor 
-    let anchor = document.createElement('a');
-    anchor.href = social_link.link;
-    anchor.textContent = social_link.name;
-
-    // Añade el anchor al botón 
-    button.appendChild(anchor);
 
     // Añade el botón al div 
     card_social_links.appendChild(button);
